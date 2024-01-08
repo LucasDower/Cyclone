70261b7e92f4cc4c294fa25830ab0508694d504e
Lucas Dower <lucasjdower@gmail.com>
2024-01-08 16:22:34 +0000

    Add loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import TimeDate from "@/components/TimeDate";
+import TimeDate from "@/components/slides/SlideTimeDate";
 import "swiper/css";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <div className="fullscreen-container">
       <Swiper>
-        <SwiperSlide className="align-center"><TimeDate></TimeDate></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide><TimeDate></TimeDate></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
       </Swiper>
     </div>
   )
